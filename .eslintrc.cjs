964e1e77cac4123d971099592bc7cc1d17db510c
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-10-07 10:10:06 -0400

    integracion de swagger y jsdoc

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'no-console': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
