001972676b11f3fa0230bf9de401186901e7fb76
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-12-08 21:26:16 -0400

    despliegue

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,23 +1,22 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-import dbConfig from "../config/db";
-import env from "../config/env";
-let ssl: {
-  require: true,
-  rejectUnauthorized: false,
-} | undefined = undefined
-if (env.env === "prod" || env.env === "test") {
+import dbConfig from '../config/db';
+import env from '../config/env';
+let ssl:
+  | {
+      require: true;
+      rejectUnauthorized: false;
+    }
+  | undefined = undefined;
+if (env.env === 'prod' || env.env === 'test') {
   ssl = {
     require: true,
     rejectUnauthorized: false,
-  }
+  };
 }
 const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.pass, {
   dialect: 'postgres',
   host: dbConfig.host,
-  dialectOptions: {
-    ssl 
-  },
   logging: false,
 });
 
